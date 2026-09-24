da08296dbc4d488c47a1b534ea41e4dc107fca1b
YesYouwin <garenagaming32@gmail.com>
2026-09-25 02:02:28 +0530

    Added Calculator Loop Answer Function For Multiple Commits

--- java/Main/src/Calculator.java
+++ java/Main/src/Calculator.java
@@ -6,28 +6,37 @@ public class Calculator {
         // declaration like response = 0 or remainder = 0 {remainder for diivsion}
         double res = 0;
         double rem = 0;
+        boolean hasResult = false;
+        String response = "No";
 
         // This shit does the user input, U make a scanner with new scanner(System.in) import it's java.util.Scanner and then
         // You make prompts for user input
 
         Scanner scanner = new Scanner(System.in);
-            System.out.print("Enter a number:");
+            System.out.print("\u001B[0mEnter a number: ");
         String first_number = scanner .nextLine();
-            System.out.print("Enter the action: ");
-        String action = scanner .nextLine();
-            System.out.print("Enter another number: ");
-        String second_number = scanner .nextLine();
-            System.out.print("Great! Do you want to do " + first_number + action + second_number + " ? (yes/no): ");  
-        String response = scanner.nextLine();
-        scanner.close();
-
-        // Basically a string to number specifically decimal -ish number converter with the use of "double". The parse is a new thing I just found out about
         double f = Double.parseDouble(first_number);
-        double s = Double.parseDouble(second_number);
 
-        // if yes 
-        // Also equals.IgnoreCase ignores the type of character like lowercase or uppercase so the code doesn't end if it receives a dumb response
-        if (response.equalsIgnoreCase("yes")) {
+        while (!response.equalsIgnoreCase("Yes")) {
+            if (hasResult) {
+                f = res;
+            }
+            System.out.print("\u001B[0mEnter The Action That Will Occur With " + f + ": ");
+                String action = scanner .nextLine();
+                if (!(action.equals("+") || action.equals("-") || action.equals("/") || action.equals("x"))) {
+                        System.out.println("\u001B[31mYou Entered An Invalid Action.");
+                        System.out.println("Allowed Actions are { + , - , / , x }");
+                        continue;
+                    }
+                
+            System.out.print("Enter another number: ");
+                String second_number = scanner .nextLine();
+
+            // Basically a string to number specifically decimal -ish number converter with the use of "double". The parse is a new thing I just found out about
+            double s = Double.parseDouble(second_number);
+
+            // if yes 
+            // Also equals.IgnoreCase ignores the type of character like lowercase or uppercase so the code doesn't end if it receives a dumb response
             if (action.equals("+")) {
                 res = f + s;
                 System.out.println("Result: " + res);
@@ -36,22 +45,27 @@ public class Calculator {
                 res = f - s;
                 System.out.println("Result: " + res);
             }
-            else if (action.equals("*")) {
+            else if (action.equals("x")) {
                 res = f * s;
                 System.out.println("Result: " + res);
             }
             else if (action.equals("/")) {
+                if (s == 0) {
+                    System.out.println("\u001B[31mError: Cannot divide by zero.");
+                    continue;
+                }
                 res = f / s; 
                 rem = f % s;
                 System.out.println("Result is " + res + " with a remainder of " + rem);
             }
+
+            hasResult = true;
+            
+            System.out.print("Are you finished? (yes/no): ");  
+                response = scanner.nextLine();
         }
-        else if (response.equals("no")) {
-            System.out.println("Well Fuck You Then.");
-        }
-        else {
-            System.out.println("Please use the fucking options.");
-        }
+        scanner.close();
+        System.out.println("\u001B[32mGreat! Your Final Answer Is: " + res);
+        System.out.println("\u001B[0m");
     }
-    
 }
